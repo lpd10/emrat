7cca1da136de030a75e81bb96a176d5e3f08191f
lpd10 <lpd@10g.pl>
2025-05-23 12:13:50 +0200

    Add files via upload

---
+++ js/sendmail.php
@@ -0,0 +1,94 @@
+<?php
+// Who you want to recieve the emails from the form.
+$sendto = 'komp03@o2.pl';
+
+// The subject you'll see in your inbox
+$subject = 'Wiadomość ze strony EMRAT';
+
+// Message for the user when he/she doesn't fill in the form correctly.
+$errormessage = 'Wykryto puste pola. Spróbuj jeszcze raz.&nbsp;';
+
+//Message for the user when he/she fills in the form correctly.
+$thanks = "Dziękujemy za wiadomość. Odezwiemy się wkrótce z odpowiedzią.&nbsp;";
+
+// Message for the bot when it fills in in at all.
+$honeypot = "You filled in the honeypot! If you're human, try again!&nbsp;";
+
+// Various messages displayed when the fields are empty.
+$emptyname =  'Wpisz imię.&nbsp;';
+$emptyemail = 'Wpisz swój adres email.&nbsp;';
+$emptymessage = 'Wpisz treść wiadomości.&nbsp;';
+$emptyphone = 'Wpisz numer telefonu.&nbsp;';
+
+// Various messages displayed when the fields are incorrectly formatted.
+$alertname =  'Wpisz swoje imię używając znaków standarowego alfabetu&nbsp;';
+$alertemail = 'Wpisz swój email podobnym formacie: <i>imie@przyklad.pl</i>?&nbsp;';
+$alertmessage = "Sprawdź czy w treści wiadomości nie używasz znaków nietypowych znaków.&nbsp;";
+
+$alert = '';
+$pass = 0;
+
+function clean_var($variable) {
+	$variable = strip_tags(stripslashes(trim(rtrim($variable))));
+  return $variable;
+}
+
+if ( empty($_REQUEST['last']) ) {
+
+  if ( empty($_REQUEST['name']) ) {
+	$pass = 1;
+	$alert .= "<li>" . $emptyname . "</li>";
+	$alert .= "<script>jQuery(\"#name\").addClass(\"error\");</script>";
+  } elseif ( preg_match( "/[][{}()*+?.\\^$|]/", $_REQUEST['name'] ) ) {
+	$pass = 1;
+	$alert .= "<li>" . $alertname . "</li>";
+  }
+  if ( empty($_REQUEST['email']) ) {
+	$pass = 1;
+	$alert .= "<li>" . $emptyemail . "</li>";
+	$alert .= "<script>jQuery(\"#email\").addClass(\"error\");</script>";
+  } elseif ( !preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/", $_REQUEST['email']) ) {
+	$pass = 1;
+	$alert .= "<li>" . $alertemail . "</li>";
+  }
+  if ( empty($_REQUEST['message']) ) {
+	$pass = 1;
+	$alert .= "<li>" . $emptymessage . "</li>";
+	$alert .= "<script>jQuery(\"#message\").addClass(\"error\");</script>";
+  } elseif ( preg_match( "/[][{}()*+?\\^$|]/", $_REQUEST['message'] ) ) {
+	$pass = 1;
+	$alert .= "<li>" . $alertmessage . "</li>";
+  }
+
+  if ( $pass==1 ) {
+
+  echo "<script>$(\".message\").toggle();$(\".message\").toggle().hide(\"fast\").show(\"fast\"); </script>";
+  echo "<script>$(\".message .alert\").addClass('alert-danger').removeClass('alert-success'); </script>";
+  echo $errormessage;
+  echo $alert;
+
+  } elseif (isset($_REQUEST['message'])) {
+
+	$message = "Od: " . clean_var($_REQUEST['name']) . "\n";
+	$message .= "Email: " . clean_var($_REQUEST['email']) . "\n";
+	$message .= "Telefon: " . clean_var($_REQUEST['phone']) . "\n";
+	$message .= "Wiadomość: \n" . clean_var($_REQUEST['message']);	
+	$header = 'Od:'. clean_var($_REQUEST['email']);
+
+	mail($sendto, $subject, $message, $header);
+	echo "<script>$(\".message\").toggle();$(\".message\").toggle().hide(\"fast\").show(\"fast\");$('#contactForm')[0].reset();</script>";
+	echo "<script>$(\".message .alert\").addClass('alert-success').removeClass('alert-danger'); </script>";
+	echo $thanks;
+	echo "<script>jQuery(\"#name\").removeClass(\"error\");jQuery(\"#email\").removeClass(\"error\");jQuery(\"#message\").removeClass(\"error\");</script>";
+	echo "<script>$(\".message .alert\").delay(4000).hide(\"fast\");</script>";
+	die();
+
+	echo "<br/><br/>" . $message;
+
+	}
+
+} else {
+	echo "<script>$(\".message\").toggle();$(\".message\").toggle().hide(\"fast\").show(\"fast\");</script>";
+	echo $honeypot;
+}
+?>
---
+++ mail/contact_me.php
@@ -0,0 +1,60 @@
+<?php
+/*
+na razie to nie potrzebne bo callback z google robi co trzeba
+//@author = mycodde.blogspot.com
+require_once "recaptchalib.php";
+// reCAPTCHA supported 40+ languages listed here: https://developers.google.com/recaptcha/docs/language
+$lang = "pl";
+// The response from reCAPTCHA
+$resp = null;
+// The error code from reCAPTCHA, if any
+$error = null;
+$reCaptcha = new ReCaptcha("6Le1DRsTAAAAABigv4RWZu27W9WwrTtEGhxrmprh");
+if ($_POST["g-recaptcha-response"]) {
+    $resp = $reCaptcha->verifyResponse(
+        $_SERVER["REMOTE_ADDR"],
+        $_POST["g-recaptcha-response"]
+    );
+   // print_r($resp); //To See the response object uncomment this
+} else {
+    echo "Recaptcha Not submitted";
+	echo "mail stop";
+	return false;
+}
+if ($resp != null && $resp->success) {
+    echo "Recaptcha Verification Success";
+    //Write other FORM password and Email Validation Procedures
+} else {
+    echo "Recaptcha Verification Error";
+}
+*/
+
+// Check for empty fields
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['phone']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+	
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+
+
+// Create the email and send the message
+$to = 'lpd@10g.pl'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Nowa wiadomość ze strony EMRAT";
+$email_body = "Szczegóły wiadomości:\n\nImię: $name\n\nEmail: $email_address\n\nNr Telefonu: $phone\n\nWiadomość:\n$message";
+$headers = "From: WWWemratautomatyka.pl\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers.= "Content-Type: text/plain;charset=utf-8\r\n";
+$headers .= "Reply-To: $email_address";	
+mail($to,$email_subject,$email_body,$headers);
+return true;			
+?>
+
